f2684f742575d0d6700678bfce192d267ec3bb73
willyaugusto <willyaugusto@gmail.com>
2017-05-01 19:34:25 -0300

    Lista de filhos

--- src/domain/Filho/filho-service.ts
+++ src/domain/Filho/filho-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
-//import { PosicaoGlobal } from "../PosicaoGlobal/posicaoglobal";
+import { PosicaoGlobal } from "../PosicaoGlobal/posicaoglobal";
 
 import { Filho } from "./Filho";
 
@@ -14,7 +14,13 @@ export class FilhoService{
         let Filhos: Filho[] = []; 
 
         //obter Filhos disponíveis para o motorista
-        
+        Filhos.push(new  Filho(1, 1,'Mario Bross', 'assets/img/mario.png', 
+            true, true, (new PosicaoGlobal(37.774929, -122.419416))));
+        Filhos.push(new  Filho(2, 2, 'Luigi Silva', 'assets/img/luigi.png', 
+            true, true, (new PosicaoGlobal(19.075984, 72.877656))));
+        Filhos.push(new  Filho(3, 3, 'Yoshi Dino', 'assets/img/Yoshi.png', 
+            true, true, (new PosicaoGlobal(35.689487, 139.691706))));
+ 
         
         return Filhos;
         
