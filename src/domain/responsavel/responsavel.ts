3534d3fa4b0ef26f3637fa09aac92e07e24bf43b
willyaugusto <willyaugusto@gmail.com>
2017-05-02 22:35:13 -0300

    adiçao de responsavel.ts

---
+++ src/domain/responsavel/responsavel.ts
@@ -0,0 +1,9 @@
+export class Responsavel{
+
+    constructor (
+        public idResposanvel: number,
+        public telefone: number,
+        public senha: string,
+        public nome: string
+    ){}
+}
